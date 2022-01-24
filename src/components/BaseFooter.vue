dab7bdbb2a679f187c9eae1ffcbfafed8844930f
Rotiken Gisa <shadrqen@outlook.com>
2022-01-24 05:24:23 +0300

    feat: added the base layout with a header and a footer

---
+++ src/components/BaseFooter.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-app>
+    <v-footer
+      class="footer"
+      color="#344754"
+    >
+      <v-container>
+        <v-row
+          justify="center"
+          no-gutters
+        >
+          <v-col
+            class="text-center white--text copyright text-caption text-xl-caption text-lg-caption text-md-caption text-sm-caption"
+            cols="12"
+          >
+            &copy; {{ new Date().getFullYear() }} MovieX. All rights reserved
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'BaseFooter'
+}
+</script>
+
+<style lang="scss" scoped>
+.footer {
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  text-align: center;
+}
+</style>
---
+++ src/components/BaseHeader.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app-bar
+    app
+    class="app_bar elevation-1"
+    dark
+    dense
+    height="50%"
+  >
+    <v-container>
+      <v-toolbar-title class="primary-toolbar-title">
+        MoviesX
+      </v-toolbar-title>
+    </v-container>
+  </v-app-bar>
+</template>
+
+<script>
+import Vue from 'vue'
+export default Vue.extend({
+  name: 'BaseHeader'
+})
+</script>
+
+<style lang="scss" scoped>
+</style>
